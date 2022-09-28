77183c78a03d7fb9499c88a30f611028ba71fa33
Mog Rouhi <moghadase.roohi@gmail.com>
2022-09-28 22:05:41 +0100

    complete five iterations and add some css

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
@@ -22,4 +24,26 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get("/beers", (req, res)=> {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log(beersFromApi); 
+  res.render('beers', {beersFromApi});
+})
+.catch(error => 
+  console.log(error)); })
+
+app.get("/random-beer", (req, res)=> {
+    punkAPI
+    .getRandom()
+    .then(beersFromApi => {
+      console.log(beersFromApi); 
+    res.render('random-beer', {beersFromApi});
+  })
+  .catch(error => 
+    console.log(error)); })
+
+
+
+app.listen(3001, () => console.log('🏃‍ on port 3001'));
